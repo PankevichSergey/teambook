2d9c61a807ce9aa2cd38375ef997be8e61004947
PankevichSergey <39384957+PankevichSergey@users.noreply.github.com>
2021-06-19 05:20:40 +0300

    Create comb.cpp

---
+++ project_code/math/comb.cpp
@@ -0,0 +1,23 @@
+
+namespace comb {
+    M f[N];
+    M fr[N];
+    void calcf() {
+        f[0] = 1;
+        for (int i = 1; i < N; ++i)
+            f[i] = f[i - 1] * i;
+        fr[N - 1] = M(1) / f[N - 1];
+        for (int i = N - 2; i >= 0; --i)
+            fr[i] = fr[i + 1] * (i + 1);
+    }
+    
+    M C(int n, int k) {
+        if (k > n || k < 0 || n < 0)
+            return 0;
+        return f[n] * fr[k] * fr[n - k];
+    }
+    M S(int n, int k, int mn) {
+        if (n == 0 && k == 0) return 1;
+        return C(n - (mn - 1) * k - 1, k - 1);
+    }
+}
