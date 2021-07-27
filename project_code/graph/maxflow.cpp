46f4fb9e8c43f3c12646dc5d90010f6748ce8a5e
PankevichSergey <39384957+PankevichSergey@users.noreply.github.com>
2021-07-28 02:46:13 +0300

    Update maxflow.cpp

--- project_code/graph/maxflow.cpp
+++ project_code/graph/maxflow.cpp
@@ -1,75 +1,85 @@
-struct edge {
-    int to;
-    int f;
-    int c;
-    edge (int aa, int bb, int cc) {
-        to = aa;
-        f = bb;
-        c = cc;
-    }
-};
-
-int K;
-int s, t;
-int n, m;
-vector<edge> edges;
-vi g[MX];
-int dist[MX];
-
-bool bfs() {
-    queue<int> q;
-    q.push(s); 
-    fill(dist, dist + n, INF);
-    dist[s] = 0;
-    while (!q.empty()) {
-        int v = q.front();
-        q.pop();
-        fx(id, g[v]) {
-            int to = edges[id].to;
-            int can = edges[id].c - edges[id].f;
-            if (can < K || dist[to] != INF) continue;
-            dist[to] = dist[v] + 1;
-            if (to == t)
-                return true;
-            q.push(to);
+struct dinic {
+    struct edge {
+        int to;
+        int cap;
+        int flow;
+        edge(int _to, int _cap, int _flow): to(_to), cap(_cap), flow(_flow) {}
+        int can() {
+            return cap - flow;
         }
+    };
+    
+    int n;
+    int s;
+    int t;
+    vector<vector<int>> g;
+    vector<edge> e;
+    dinic() {}
+    dinic(int _n, int _s, int _t) {
+        n = _n;
+        s = _s;
+        t = _t;
+        g.resize(n);
     }
-    return false;
-}
+    void add_edge(int a, int b, int cap) {
+        g[a].pb(e.size());
+        e.pb(edge(b, cap, 0));
+        g[b].pb(e.size());
+        e.pb(edge(a, 0, 0));
+    }
+    void push(int id, int flow) {
+        e[id].flow += flow;
+        e[id ^ 1].flow -= flow;
+    } 
 
-int it[MX];
+    const int INF = 1e9 + 228;
+    vector<int> dist;
+    bool bfs() {
+        dist.assign(n, INF);
+        dist[s] = 0;
+        queue<int> q;
+        q.push(s);
+        while (q.size()) {
+            int v = q.front();        
+            q.pop();
+            for (int id : g[v]) {
+                if (e[id].can() && dist[e[id].to] == INF) {
+                    dist[e[id].to] = dist[v] + 1;
+                    q.push(e[id].to);
+                }
 
-int dfs(int v, int mn = INF) {
-    if (v == t)
-        return mn;
-    while (it[v] != siz(g[v])) {
-        int id = g[v][it[v]];
-        int to = edges[id].to;
-        int can = edges[id].c - edges[id].f;
-        if (can >= K && dist[to] == dist[v] + 1) {
-            int res = dfs(to, min(mn, can));
-            if (res) {
-                edges[id].f += res;
-                edges[id^1].f -= res;
-                return res;
             }
         }
-        ++it[v];
+        return dist[t] < INF;
     }
-    return 0;
-}
 
-int maxflow() {
-    int flow = 0;
-    K = 1 << 30;
-    while (K) {
-        while (bfs()) {
-            memset(it, 0, sizeof(int) * n);
-            int delta;
-            while (delta = dfs(s))
-                flow += delta;
+    vector<int> ptr;
+    int dfs(int v, int curmin) {
+        if (v == t) return curmin;
+        while (ptr[v] < g[v].size()) {
+            int id = g[v][ptr[v]];
+            int to = e[id].to;
+            if (e[id].can() && dist[to] == dist[v] + 1) {
+                int can = dfs(to, min(curmin, e[id].can()));
+                if (can) {
+                    push(id, can);
+                    return can;
+                }
+            }
+            ++ptr[v];
         }
-        K >>= 1;
+        return 0;
     }
-    return flow;
-}
+    int maxflow() {
+        int flow = 0;
+        while (bfs()) {
+            ptr.assign(n, 0);
+            int delta;
+            while (delta = dfs(s, INF)) {
+                flow += delta;                
+            }            
+        }       
+        return flow;
+    }
+    
+};
