697c7331a7b1ddee0910253e62ca45ff81cc7890
PankevichSergey <39384957+PankevichSergey@users.noreply.github.com>
2021-08-05 15:00:50 +0300

    Update template.cpp

--- project_code/template.cpp
+++ project_code/template.cpp
@@ -18,7 +18,7 @@
 #include <climits>
 #include <ctime>
 #include <chrono>
-
+#include <unordered_map>
 
 /*
 #pragma GCC optimize("Ofast")
@@ -41,12 +41,7 @@
 #define fi(a, b) for (int i = a; i < b; i++)
 #define fj(a, b) for (int j = a; j < b; j++)
 #define fk(a, b) for (int k = a; k < b; k++)
-#define fi1(a, b) for (int i = a - 1; i >= b; i--)
-#define fj1(a, b) for (int j = a - 1; j >= b; j--)
-#define fk1(a, b) for (int k = a - 1; k >= b; k--)
 #define fx(x, a) for (auto& x : a)
-#define rep(i, a, b) for (int i = a; i < b; ++i)
-#define rep1(i, a, b) for (int i = a - 1; i >= b; --i)
 #define siz(x) (int)x.size()
 #define lb lower_bound
 #define ub upper_bound
@@ -60,35 +55,49 @@ template<typename T1, typename T2>inline void mine(T1 &x, const T2 &y) { if (y <
 template<typename T1, typename T2>inline void maxe(T1 &x, const T2 &y) { if (x < y) x = y; }
 
 template<typename T> ostream& operator << (ostream &os, const vector<T> &b) {
-    for (auto &k : b) os << k << ' ';
+    for (const auto &k : b) os << k << ' ';
     return os;
 }
 template<typename T> istream& operator >> (istream &is, vector<T> &b) {
     for (auto &k : b) is >> k;
     return is;
 }
+template<typename T> ostream& operator << (ostream &os, const set<T> & b) {
+    for (const auto &k : b) os << k << ' ';
+    return os;
+}
+template<typename T1, typename T2> ostream& operator << (ostream & os, const pair<T1, T2> &b) {
+    os << b.first << ' ' << b.second;
+    return os;
+}
 
 
 typedef long long ll;
 typedef unsigned long long ull;
 typedef long double ld;
-typedef char ch;
 typedef string str;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
 typedef vector<vi> vvi;
 typedef vector<pii> vpii;
-typedef vector<vpii> vvpii;
-typedef vector<ch> vch;
-typedef vector<vch> vvch;
+typedef vector<char> vch;
 typedef vector<str> vs;
+
+
 
 
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 
+void solve() {
+    
+}
 
 int32_t main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
+
+    int t = 1;
+    //cin >> t;
+    while (t--) solve();
 }
