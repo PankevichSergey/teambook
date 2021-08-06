38c95a4d539feffec6597acac7fbc21c361e9275
PankevichSergey <39384957+PankevichSergey@users.noreply.github.com>
2021-08-07 00:02:11 +0300

    Update dsu.cpp

--- project_code/data_structures/dsu.cpp
+++ project_code/data_structures/dsu.cpp
@@ -1,19 +1,22 @@
-int p[MX];
-int sz[MX];
-
-int get(int v) {
-    if (p[v] == v)
-        return v;
-    return p[v] = get(p[v]);
-}
-
-void unite(int a, int b) {
-    a = get(a);
-    b = get(b);
-    if (a == b)
-        return;
-    if (sz[a] < sz[b])
-        swap(a, b);
-    sz[a] += sz[b];
-    p[b] = a;
-}
+struct dsu {
+    vector<int> pr;
+    vector<int> sz;
+    dsu(int n) {
+        pr.resize(n);
+        iota(all(pr), 0);
+        sz.assign(n, 1);
+    }
+    int get(int v) {
+        if (pr[v] == v) return v;
+        return pr[v] = get(pr[v]);
+    }
+    bool unite(int a, int b) {
+        a = get(a);
+        b = get(b);
+        if (a == b) return false;
+        if (sz[a] < sz[b]) swap(a, b);
+        sz[a] += sz[b];
+        pr[b] = a;
+        return true;
+    }
+};
