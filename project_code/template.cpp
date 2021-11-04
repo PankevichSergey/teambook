ffbc9673c1b43af29227f60a68756877a9b87e55
PankevichSergey <39384957+PankevichSergey@users.noreply.github.com>
2021-11-04 18:47:53 +0300

    Update template.cpp

--- project_code/template.cpp
+++ project_code/template.cpp
@@ -19,74 +19,149 @@
 #include <ctime>
 #include <chrono>
 #include <unordered_map>
-
-/*
-#pragma GCC optimize("Ofast")
-#pragma GCC optimize("O3")
-#pragma GCC optimize("unroll-loops")
-#pragma GCC target("sse,sse2,sse3,ssse3,sse4,sse4.1,sse4.2,popcnt,abm,mmx,avx,avx2,tune=native")
-*/
+#include <complex>
 
 
+using namespace std;
+
+// floating point errors
+// #pragma GCC optimize("Ofast,unroll-loops")
+
+// safe
+// #pragma GCC optimize("O3,unroll-loops")
+
+
+
+
+// doesn't work in yandex
+// #pragma GCC target("avx2,bmi,bmi2,lzcnt,tzcnt,popcnt")
+
+// doesn't work in some judges
+// #pragma GCC target(“avx2,bmi,bmi2,lzcnt,tzcnt,popcnt,tune=native”)
+
+// safe
+// #pragma GCC target("sse4.2,bmi,bmi2,lzcnt,tzcnt,popcnt")
+
+
+
+
+template<typename T1, typename T2>inline void mine(T1 &x, const T2 &y) { if (y < x) x = y; }
+template<typename T1, typename T2>inline void maxe(T1 &x, const T2 &y) { if (x < y) x = y; }
+
+template<typename T1, typename T2> ostream& operator << (ostream & os, const pair<T1, T2> &b) {
+    os << b.first << ' ' << b.second;
+    return os;
+}
+
+template<typename T> ostream& operator << (ostream &os, const vector<T> &b) {
+    for (const auto &k : b) os << k << ' ';
+    return os;
+}
+
+template<typename T> istream& operator >> (istream &is, vector<T> &b) {
+    for (auto &k : b) is >> k;
+    return is;
+}
+
+template<typename T> ostream& operator << (ostream &os, const set<T> & b) {
+    for (const auto &k : b) os << k << ' ';
+    return os;
+}
+
+
+void dbg_out() { cerr << endl; }
+ 
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) {
+  cerr << "   " << H;
+  dbg_out(T...);
+}
+ 
 #ifdef LOCAL
-    #define dbg(x) cout << #x << " : " << x << endl;
+#define dbg(...) cerr << "[" << #__VA_ARGS__ << "]:", dbg_out(__VA_ARGS__)
 #else
-    #define dbg(x)
+#define dbg(...)
 #endif
 
-#define int long long
+
+
+
 #define pb push_back
+#define eb emplace_back
 #define ppb pop_back()
 #define mp make_pair
-#define fi(a, b) for (int i = a; i < b; i++)
-#define fj(a, b) for (int j = a; j < b; j++)
-#define fk(a, b) for (int k = a; k < b; k++)
+#define fi first;
+#define se second;
+#define fr front()
+#define bc back()
 #define fx(x, a) for (auto& x : a)
-#define siz(x) (int)x.size()
+#define siz(x) (long long)x.size()
 #define lb lower_bound
 #define ub upper_bound
 #define all(x) x.begin(), x.end()
 #define left leftkekmda
 #define right rightkekmda
 
-using namespace std;
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using pld = pair<ld, ld>;
+using str = string;
 
-template<typename T1, typename T2>inline void mine(T1 &x, const T2 &y) { if (y < x) x = y; }
-template<typename T1, typename T2>inline void maxe(T1 &x, const T2 &y) { if (x < y) x = y; }
+template<typename T> using V = vector<T>; 
+using vi = V<int>;
+using vch = V<char>;
+using vb = V<bool>;
+using vl = V<ll>;
+using vld = V<ld>;
+using vs = V<str>;
+using vpii = V<pii>;
+using vpll = V<pll>;
+using vpld = V<pld>;
 
-template<typename T> ostream& operator << (ostream &os, const vector<T> &b) {
-    for (const auto &k : b) os << k << ' ';
-    return os;
+using vvi = V<vi>;
+using vvch = V<vch>;
+using vvb = V<vb>;
+using vvl = V<vl>;
+using vvld = V<vld>;
+using vvs = V<vs>;
+using vvpii = V<vpii>;
+using vvpll = V<vpll>;
+using vvpld = V<vpld>;
+
+
+
+
+
+template<typename T>
+int Bit(T x, int sh) {
+    return (x >> sh) & 1;
 }
-template<typename T> istream& operator >> (istream &is, vector<T> &b) {
-    for (auto &k : b) is >> k;
-    return is;
+template<typename T>
+int Bits(T x) {
+    return __builtin_popcountll(x);
 }
-template<typename T> ostream& operator << (ostream &os, const set<T> & b) {
-    for (const auto &k : b) os << k << ' ';
-    return os;
+template<typename T>
+int FirstBit(T x) {
+    return __builtin_ctzll(x);
 }
-template<typename T1, typename T2> ostream& operator << (ostream & os, const pair<T1, T2> &b) {
-    os << b.first << ' ' << b.second;
-    return os;
+template<typename T>
+int LastBit(T x) {
+    return 63 - __builtin_clzll(x);
 }
 
-
-typedef long long ll;
-typedef unsigned long long ull;
-typedef long double ld;
-typedef string str;
-typedef pair<int, int> pii;
-typedef vector<int> vi;
-typedef vector<vi> vvi;
-typedef vector<pii> vpii;
-typedef vector<char> vch;
-typedef vector<str> vs;
-
+ll div_up(ll a, ll b) { return a/b+((a^b)>0&&a%b); }
+ll div_down(ll a, ll b) { return a/b-((a^b)<0&&a%b); }
 
 
 
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+
+
 
 void solve() {
     
@@ -95,7 +170,6 @@ void solve() {
 int32_t main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    cout.tie(0);
 
     int t = 1;
     //cin >> t;
