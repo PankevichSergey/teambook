b1819e0d0db26ae1acf2b80a297052510308f017
PankevichSergey <39384957+PankevichSergey@users.noreply.github.com>
2022-04-04 18:19:48 +0300

    Update maxflow.cpp

--- project_code/graph/maxflow.cpp
+++ project_code/graph/maxflow.cpp
@@ -8,31 +8,28 @@ struct dinic {
             return cap - flow;
         }
     };
-    
+    vector<edge> e;
+    vector<vector<int>> g;
     int n;
     int s;
     int t;
-    vector<vector<int>> g;
-    vector<edge> e;
-    dinic() {}
-    dinic(int _n, int _s, int _t) {
-        n = _n;
-        s = _s;
-        t = _t;
+    dinic(int _n) {
+        n = _n + 2;
+        s = n - 1;
+        t = n - 2;
         g.resize(n);
     }
     void add_edge(int a, int b, int cap) {
-        g[a].pb(e.size());
-        e.pb(edge(b, cap, 0));
-        g[b].pb(e.size());
-        e.pb(edge(a, 0, 0));
+        g[a].push_back(e.size());
+        e.push_back(edge(b, cap, 0));
+        g[b].push_back(e.size());
+        e.push_back(edge(a, 0, 0));
     }
-    void push(int id, int flow) {
-        e[id].flow += flow;
-        e[id ^ 1].flow -= flow;
-    } 
-
-    const int INF = 1e9 + 228;
+    void flow(int id, int x) {
+        e[id].flow += x;
+        e[id ^ 1].flow -= x;
+    }
+    const int INF = 1e9 + 50;
     vector<int> dist;
     bool bfs() {
         dist.assign(n, INF);
@@ -40,44 +37,46 @@ struct dinic {
         queue<int> q;
         q.push(s);
         while (q.size()) {
-            int v = q.front();        
+            int v = q.front();
             q.pop();
             for (int id : g[v]) {
-                if (e[id].can() && dist[e[id].to] == INF) {
-                    dist[e[id].to] = dist[v] + 1;
-                    q.push(e[id].to);
+                if (e[id].can()) {
+                    int to = e[id].to;
+                    if (dist[to] == INF) {
+                        dist[to] = dist[v] + 1;
+                        q.push(to);
+                    }
                 }
-
             }
         }
         return dist[t] < INF;
     }
-    vector<int> ptr;
+    vector<int> it;
     int dfs(int v, int curmin) {
         if (v == t) return curmin;
-        while (ptr[v] < g[v].size()) {
-            int id = g[v][ptr[v]];
+        while (it[v] < g[v].size()) {
+            int id = g[v][it[v]];
             int to = e[id].to;
             if (e[id].can() && dist[to] == dist[v] + 1) {
-                int can = dfs(to, min(curmin, e[id].can()));
-                if (can) {
-                    push(id, can);
-                    return can;
+                int res = dfs(to, min(curmin, e[id].can()));
+                if (res) {
+                    flow(id, res);
+                    return res;
                 }
-            }
-            ++ptr[v];
+            } 
+            ++it[v];
         }
         return 0;
     }
     int maxflow() {
-        int flow = 0;
+        int res = 0;
         while (bfs()) {
-            ptr.assign(n, 0);
             int delta;
+            it.assign(n, 0);
             while (delta = dfs(s, INF)) {
-                flow += delta;                
-            }            
-        }       
-        return flow;
+                res += delta;
+            } 
+        }
+        return res;
     }
 };
