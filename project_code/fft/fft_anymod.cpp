a1d8d0e164701f9276a7a3334cd2df95ef48d971
PankevichSergey <39384957+PankevichSergey@users.noreply.github.com>
2022-08-14 19:35:57 +0300

    Update fft_anymod.cpp

--- project_code/fft/fft_anymod.cpp
+++ project_code/fft/fft_anymod.cpp
@@ -1,24 +1,53 @@
+const int MOD = 1'000'000'007;
+
+inline int mult(int x, int y) {
+    return 1ll * x * y % MOD;
+}
+
+inline int add(int x, int y) {
+    x += y;
+    if (x >= MOD) return x - MOD;
+    return x;
+}
+
+const int LOG = 20;
+const int N = 1 << LOG;
+const int NN = N + 5;
+
+const int BASE = 1 << 15;
+const int LOGB = 15;
+const int Q[]{1, BASE, static_cast<int>(1ll * BASE * BASE % MOD)};
+const ld PI = 4 * atan(1);
+using cd = complex<long double>;
+
+
 int bitrev[NN];
 cd w[NN];
 
-void init() {
-    fi(0, N) {
+
+#warning call initfft
+bool init_was_called = false;
+void initfft() {
+    init_was_called = true;
+    for (int i = 0; i < N; ++i) {
         bitrev[i] = ((i & 1) << (LOG - 1)) ^ (bitrev[i >> 1] >> 1);
         w[i] = complex(cos(2 * PI * i / N), sin(2 * PI * i / N));
     }
 }
- 
+
 void fft(vector<cd> & a, int k) {
+    assert(init_was_called);
     int L = 1 << k;
-    fi(0, L) {
+    for (int i = 0; i < L; ++i) {
         int x = bitrev[i] >> (LOG - k);
-        if (i > x)
+        if (i > x) {
             swap(a[i], a[x]);
+        }
     }
-    rep(lvl, 0, k) {
+    for (int lvl = 0; lvl < k; ++lvl) {
         int len = 1 << lvl;
         for (int i = 0; i < L; i += len << 1) {
-            fj(0, len) {
+            for (int j = 0; j < len; ++j) {
                 cd x = a[i + j];
                 cd y = a[i + j + len] * w[j << (LOG - 1 - lvl)];
                 a[i + j] = x + y;
@@ -27,54 +56,54 @@ void fft(vector<cd> & a, int k) {
         }
     }
 }
- 
+
 void invfft(vector<cd> & a, int k) {
     fft(a, k);
     int L = 1 << k;
     reverse(a.begin() + 1, a.begin() + L);
-    fx(x, a) {
+    for (auto &x : a) {
         x /= L;
         x =  cd(((long long)round(x.real())) % MOD, 0);
     }
 }
 
-void norm(vector<ll> & x) {
-    while (siz(x) && !x.back())
-        x.ppb;
-}
-
-vector<ll> mult(vector<ll> & a, vector<ll> & b) {
-    norm(a);
-    norm(b);
+vector<ll> mult(const vector<ll> & a, const vector<ll> & b) {
+    if (a.empty() && b.empty()) {
+        return {};
+    }
+    int nsz = a.size() + b.size() - 1;
     int k = 0;
-    while ((1 << k) < siz(a) + siz(b) - 1)
+    while ((1 << k) < nsz) {
         ++k;
+    }
     int L = 1 << k;
-    vector<vector<cd>> ac(2, vector<cd>(L));
-    vector<vector<cd>> bc(2, vector<cd>(L));
-    fi(0, siz(a)) {
-        ac[0][i] = a[i] & (BASE - 1);
-        ac[1][i] = a[i] >> LOGB; 
+    vector<vector<cd>> aa(2, vector<cd>(L, cd(0, 0)));
+    vector<vector<cd>> bb(2, vector<cd>(L, cd(0, 0)));
+    for (int i = 0; i < a.size(); ++i) {
+        aa[0][i] = a[i] & (BASE - 1);
+        aa[1][i] = a[i] >> LOGB;
     }
-    fi(0, siz(b)) {
-        bc[0][i] = b[i] & (BASE - 1);
-        bc[1][i] = b[i] >> LOGB;
+    for (int i = 0; i < b.size(); ++i) {
+        bb[0][i] = b[i] & (BASE - 1);
+        bb[1][i] = b[i] >> LOGB;
     }
-    fft(bc[0], k);
-    fft(bc[1], k);
-    fft(ac[0], k);
-    fft(ac[1], k);
+    fft(bb[0], k);
+    fft(bb[1], k);
+    fft(aa[0], k);
+    fft(aa[1], k);
     vector<ll> res(L, 0);
     vector<cd> c(L);
-    fi(0, 2) {
-        fj(0, 2) { 
-            rep(len, 0, L)
-                c[len] = ac[i][len] * bc[j][len];
+    for (int i = 0; i < 2; ++i) {
+        for (int j = 0; j < 2; ++j) {
+            for (int len = 0; len < L; ++len) {
+                c[len] = aa[i][len] * bb[j][len];
+            }
             invfft(c, k);
-            rep(len, 0, L)
-                res[len] = add(res[len], mult(Q[i + j], (int)round(c[len].real()) % MOD));
+            for (int len = 0; len < L; ++len) {
+                res[len] = add(res[len], mult(Q[i + j], (long long)round(c[len].real()) % MOD));
+            }
         }
     }
-    norm(res);
+    res.resize(a.size() + b.size() - 1, 0);
     return res;
 }
